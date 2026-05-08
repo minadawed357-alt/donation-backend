691d354e339c6a74f6dbd5ee19b5da3b30d30623
Ahmed Karam <ahmedferon74.com>
2026-05-08 19:05:04 +0300

    fix firebase env parsing

--- src/database/firebaseAdmin.js
+++ src/database/firebaseAdmin.js
@@ -1,10 +1,11 @@
 import admin from "firebase-admin";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 if (!admin.apps.length) {
   const serviceAccount = JSON.parse(
-    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf8")
+    process.env.FIREBASE_SERVICE_ACCOUNT
   );
 
   admin.initializeApp({
