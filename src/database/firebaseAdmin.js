61660cbced724ab3e1d212677714a42fad187c27
Ahmed Karam <ahmedferon74.com>
2026-05-08 19:00:55 +0300

    fix: use base64 for firebase credentials

--- src/database/firebaseAdmin.js
+++ src/database/firebaseAdmin.js
@@ -1,10 +1,12 @@
 import admin from "firebase-admin";
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
-const serviceAccount = require("../../serviceAccount.json");
+import dotenv from "dotenv";
+dotenv.config();
 
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, "base64").toString("utf8")
+  );
+
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
